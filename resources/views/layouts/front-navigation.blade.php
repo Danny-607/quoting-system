dc0f8dd837e2212794e0ae3adb387e6d4c2845c9
Danny-607 <danielhudson1294@gmail.com>
2024-04-18 12:04:50 +0100

    Added links to dashboard on front-end navigation

--- resources/views/layouts/front-navigation.blade.php
+++ resources/views/layouts/front-navigation.blade.php
@@ -11,29 +11,36 @@
     <div class="logo">
         <a href="{{ url('/') }}"><img src="{{asset('images/logo.png')}}" alt="Logo"></a>
     </div>
-    <ul class="nav-links right">
-    @guest
-        @if (Route::has('login'))
-        <li><a href="{{ route('login') }}">Login</a></li>
-        @endif
-        @if (Route::has('register'))
-        <li><a href="{{ route('register') }}">Register</a></li>
-        @endif
-        @else
-        <ul></ul>
-        <li> 
-            <a id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->name }}</a>
-        </li>
-        <li>
-            <a  href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> {{ __('Logout') }}</a>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-        </li>
-    
-    @endguest
-</ul>
+    <ul class="nav-links right">
+        @guest
+            @if (Route::has('login'))
+            <li><a href="{{ route('login') }}">Login</a></li>
+            @endif
+            @if (Route::has('register'))
+            <li><a href="{{ route('register') }}">Register</a></li>
+            @endif
+        @else
+            <li><a href="#" role="button">{{ Auth::user()->first_name }}</a></li>
+            <li>
+                @if(Auth::user()->hasRole('customer'))
+                <a href="{{ route('customer.index') }}">Dashboard</a>
+            @elseif(Auth::user()->hasRole('admin'))
+                <a href="{{ route('admin.index') }}">Dashboard</a>
+                @elseif(Auth::user()->hasRole('manager'))
+                <a href="{{ route('manager.index') }}">Dashboard</a>
+            @elseif(Auth::user()->hasRole('designer'))
+                <a href="{{ route('designer.index') }}">Dashboard</a>
+            @endif
+            </li>
+            <li>
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
+        @endguest
+    </ul>
 </nav>
 <main>
     @yield('content')
