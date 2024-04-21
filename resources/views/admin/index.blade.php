98d283d169bc0a347ee349aca72193eb176d89c2
Danny-607 <danielhudson1294@gmail.com>
2024-04-21 14:21:38 +0100

    Updated some styling

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,59 +1,54 @@
 @extends('layouts.dashboard')
 @section('title', 'Dashboard')
 @section('content')
-@can('view admin dashboard')
-    
-<h1>Admin Dashboard</h1>
-    <h2>Manage Users</h2>
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
+    @can('view admin dashboard')
+
+        <h1>Admin Dashboard</h1>
+        <h2>Manage Users</h2>
+        <div>
+            <table>
+                <thead>
                     <tr>
-                        <td>{{ $user->first_name }} {{ $user->last_name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ $user->roles->pluck('name')->join(', ') }}</td>
-                        <td>
-                            <div class="action-buttons">
-                                <a class="edit-btn btn" href="{{ Route('users.edit', $user->id) }}">Edit</a>
-                                <form method="POST" action="{{ route('users.destroy', $user->id) }}"
-                                    style="display: inline-block;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="delete-btn btn" type="submit">Delete</button>
-                                </form>
-                            </div>
-                        </td>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    <a class="create-btn btn" href="{{ route('users.create') }}">Create a new user</a>
-    <a class="create-btn btn" href="{{ route('roles.create') }}">Create a new role</a>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->first_name }} {{ $user->last_name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->roles->pluck('name')->join(', ') }}</td>
+                            <td>
+                                <div class="action-buttons">
+                                    <a class="edit-btn btn" href="{{ Route('users.edit', $user->id) }}">Edit</a>
+                                    <form method="POST" action="{{ route('users.destroy', $user->id) }}"
+                                        style="display: inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="delete-btn btn" type="submit">Delete</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <a class="create-btn btn" href="{{ route('users.create') }}">Create a new user</a>
+        <a class="create-btn btn" href="{{ route('roles.create') }}">Create a new role</a>
 
 
-    @if (session()->has('success'))
-        <p class="success-msg">{{ session('success') }}</p>
-    @endif
-    <section class="admin-card-container">
-
-
-    <div class="admin-card">
-
-
+        @if (session()->has('success'))
+            <p class="success-msg">{{ session('success') }}</p>
+        @endif
+        <h2>Roles and Permissions</h2>
+        <section class="admin-card-container">
             @foreach ($roles as $role)
-
                 <div class="admin-card">
-                    <h2>{{strToUpper($role->name)}}</h2>
+                    <h2>{{ strToUpper($role->name) }}</h2>
                     <ul class="">
                         @if ($role->permissions->isEmpty())
                             <li class="">No permissions assigned</li>
@@ -63,18 +58,18 @@
                             @endforeach
                         @endif
                     </ul>
-                        <a class="edit-btn btn" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-                        <form action="{{ route('roles.destroy', $role->id) }}" method="post" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn delete-btn" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
+                    <a class="edit-btn btn" href="{{ route('roles.edit', $role->id) }}">Edit</a>
+                    <form action="{{ route('roles.destroy', $role->id) }}" method="post" class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn delete-btn" onclick="return confirm('Are you sure?')">Delete</button>
+                    </form>
 
                 </div>
-            </div>
+                </div>
             @endforeach
-        </div>
-    </div>
-</section>
+
+            </div>
+        </section>
     @endcan
 @endsection
